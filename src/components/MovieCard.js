e13c073b3d407812ce803b3886e5ebe93490cf2f
lexipierson <lexi.pierson@limelightmarketing.com>
2024-10-09 08:34:29 -0500

    October 7th changes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,36 @@
 import React from "react";
 import "../styles/styles.css";
 
+// Monday, October 7th, 2024
+
 export default function MovieCard({ movie }) {
+  const handleMissingImage = (event) => {
+    event.target.src = "images/default.jpg";
+  };
+
+  const getRatingClass = (rating) => {
+    if (rating >= 8) {
+      return "rating-good";
+    } else if (rating >= 6) {
+      return "rating-ok";
+    } else {
+      return "rating-bad";
+    }
+  };
+
   return (
     <div key={movie.id} className="movie-card">
-      <img src={`images/${movie.image}`} alt={movie.title} />
+      <img
+        src={`images/${movie.image}`}
+        alt={movie.title}
+        onError={handleMissingImage}
+      />
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
-        <p className="movie-card-rating">{movie.rating}</p>
+        <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+          {movie.rating}
+        </p>
       </div>
     </div>
   );
