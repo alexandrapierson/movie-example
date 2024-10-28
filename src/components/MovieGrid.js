ffe623a27ff36bf5ef50eb8ed4cf84515578c723
lexipierson <lexi.pierson@limelightmarketing.com>
2024-10-28 08:10:17 -0500

    October 16th

--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -78,7 +78,7 @@ export default function MovieGrid({ movies, watchlist, toggleWatchlist }) {
         </div>
 
         {/* Rating Selector */}
-        <div className="filter-slot">
+        <div className="filtNer-slot">
           <label>Rating</label>
           <select className="filter-dropdown" onChange={handleRatingChange}>
             <option>All</option>
--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "../styles/styles.css";
 
-// Fiiday October 11th.
+// Friday October 11th.
+// Wednesday October 16th.
 
 export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
+  function matchWatchList(movie) {
+    return movie.watchlist;
+  }
+
+  const filteredMovies = movies.filter((movie) => matchWatchList(movie));
+
   return (
     <div>
       <h1 className="title">Your Watchlist</h1>
+
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard
+            movie={movie}
+            key={movie.id}
+            toggleWatchlist={toggleWatchlist}
+            isWatchlisted={watchlist.includes(movie.id)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
